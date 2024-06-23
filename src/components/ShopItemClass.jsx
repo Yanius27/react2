adc511649bca0122765463651e165d20f6c24576
Yanius27 <zhzh-1987@mail.ru>
2024-06-23 18:38:21 +0700

    react 2 ex 1 commit

---
+++ src/components/ShopItemClass.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export class ShopItemClass extends Component {
+  render() {
+    const {
+      item: {
+        brand,
+        title,
+        description,
+        descriptionFull,
+        price,
+        currency
+      }
+    } = this.props;
+
+    return (
+      <div class="main-content">
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div class="description">{descriptionFull}</div>
+        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{`${currency}${price}`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    )
+  }  
+}
