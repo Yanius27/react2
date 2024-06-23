8e3f3e2185e13b6f53dcd3a8bacdfcda139a69ef
Yanius27 <zhzh-1987@mail.ru>
2024-06-24 01:38:25 +0700

    react 2 ex 11 commit

--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -14,15 +14,15 @@ export class ShopItemClass extends Component {
     } = this.props;
 
     return (
-      <div class="main-content">
+      <div className="main-content">
         <h2>{brand}</h2>
         <h1>{title}</h1>
         <h3>{description}</h3>
-        <div class="description">{descriptionFull}</div>
-        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-        <div class="divider"></div>
-        <div class="purchase-info">
-          <div class="price">{`${currency}${price}`}</div>
+        <div className="description">{descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{`${currency}${price}`}</div>
           <button>Добавить в корзину</button>
         </div>
       </div>
